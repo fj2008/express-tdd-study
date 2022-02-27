aea5e1733c99cfd348d5229ce8d64892a2046f91
fj2008 <fj2008@naver.com>
2022-02-27 15:39:45 +0900

    env설정추가

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const HOST = '0.0.0.0';
 const PORT = 3000;
 const app = express();
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv');
 const productRoute = require('./routes');
+dotenv.config({});
 mongoose
     .connect(
-        'mongodb://25g:fj4951826@localhost:27017/study?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
+        `mongodb://${process.env.MONGO_ID}:${process.env.MONGO_PW}@localhost:27017/study?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
